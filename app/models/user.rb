2ff263d74dd5d2bd477ae36d61eead31856fa9b8
Cam MacDougall <cammacdougall12@gmail.com>
2021-07-26 23:43:13 +0000

    create a class method to authenticate users. To create test file to validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   validates :email, presence: true
   validates :email, :uniqueness => { :case_sensitive => false }
+
+  def self.authenticate_with_credentials(email,password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    end
+    return nil
+  end
+  
 end
